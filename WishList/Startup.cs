c584f06967b9f5abc1802d2ab257fe7f79bf9b28
Brendan Childs <Brendan.Childs@soeidental.com>
2020-01-08 10:38:01 +1300

    oops

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,9 +23,11 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-                app.UseMvcWithDefaultRoute();
+                app.UseDeveloperExceptionPage();
             else
                 app.UseExceptionHandler("/Home/Error");
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
