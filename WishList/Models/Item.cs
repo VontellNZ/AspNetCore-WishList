1ca938c59dd5f6f01b8009dcb69aa0b6c01975cb
Brendan Childs <Brendan.Childs@soeidental.com>
2020-01-08 10:56:25 +1300

    Create Item Model With EntityFramework Support

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        [Required] [MaxLength(50)]
+        public string Description { get; set; }
+        public int Id { get; set; }
+    }
+}
